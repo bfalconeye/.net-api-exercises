3b5591f442c815c6c808c857c382a6994cc73d51
bfalconeye <70069234+bfalconeye@users.noreply.github.com>
2022-07-13 22:31:39 +0300

    Files upload

---
+++ FormDemo/Shared/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FormDemo.Shared
+{
+    public class User
+    {
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
