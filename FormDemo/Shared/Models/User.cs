8fe7140c52be66b800424f2c519ee4981bcf6323
bfalconeye <70069234+bfalconeye@users.noreply.github.com>
2022-07-14 16:34:16 +0300

    files upload

---
+++ FormDemo/Shared/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FormDemo.Shared
+{
+    public class User
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FormDemoAPI/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FormDemoAPI;
+using FormDemo;
+using FormDemo.Shared;
+
+namespace FormDemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> _users = new List<User>
+        {
+            new User
+            {
+                Name="Beyza",
+                Surname="Sahingoz",
+                Email="beyzas@gmail.com",
+                Password="12321"
+
+            }
+        };
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return await Task.FromResult(Ok(_users));
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUsers(User users)
+        {
+            _users.Add(users);
+            return await Task.FromResult(Ok(_users));
+        }
+        [HttpDelete]
+        public async Task<ActionResult<List<User>>> DeleteUsers(User users)
+        {
+            _users.Remove(users);
+            return await Task.FromResult(Ok(_users));
+        }
+        }
+}
