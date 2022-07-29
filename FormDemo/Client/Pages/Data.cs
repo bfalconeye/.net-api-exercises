a87f896f45cc88fc02ebf9ccd2a59b14f8eca569
beyza <70069234+bfalconeye@users.noreply.github.com>
2022-07-29 17:33:07 +0300

    added chart

---
+++ FormDemo/Client/Pages/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace FormDemo.Client.Pages
+{
+    public class Data
+    {
+        public int id { get; set; }
+        public List<string> Names { get; set; }
+        public List<int> Values { get; set; }
+        public List<Data> datas = new List<Data>() { };
+        public List<Data> GetDatas()
+        {
+
+            datas.Add(new Data { id = 1, Names = { "A", "B" }, Values = { 3423, 1234 } });
+            datas.Add(new Data { id = 2, Names = { "C", "D" }, Values = { 5423, 7234 } });
+            return datas;
+        }
+    }
+}
---
+++ FormDemo/Client/Pages/SampleData.cs
@@ -0,0 +1,17 @@
+﻿namespace FormDemo.Client.Pages
+{
+    public class SampleData
+    {
+        Data data = new Data();
+        List<Data> datas;
+        public SampleData()
+        {
+            datas = new List<Data>();
+        }
+        public List<Data> GetDatas()
+        {
+            datas.Add(new Data { id = 1, Names = new List<string> { "A", "B", "C", "D" }, Values = new List<int> { 3423, 1234, 5423, 7234 } });
+            return datas;
+        }
+    }
+}
