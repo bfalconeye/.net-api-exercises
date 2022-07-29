7ab93e803dad47768504d749e3693001f39e40b0
beyza <70069234+bfalconeye@users.noreply.github.com>
2022-07-29 17:35:10 +0300

    added charts datas

---
+++ wwwroot/js/Datas.js
@@ -0,0 +1,15 @@
+﻿function Datas(x, y) {
+    var options = {
+        chart: {
+            type: 'donut'
+        },
+        series: x,
+        labels: y
+    }
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
+}
+
+
